6a2a80133529ac6b4e57e99429dba16f61546b28
Duncan Cragg <duncan.cragg@tesglobal.com>
2017-08-03 11:28:17 +0100

    First cut towards todomvc

---
+++ examples/todomvc/evaluatetodo.jsx
@@ -0,0 +1,27 @@
+
+import Forest from 'forest';
+import renderers from 'rendertodo';
+
+Forest.store(
+  [
+    { UID: 'uid-1',                     is: 'guistack',   name: 'TodoMVC', list: ['uid-3', 'uid-2'] },
+    { UID: 'uid-2', evaluate: evalTodo, is: 'todomvc', counter: 42, topic: 'banana', watching: 'uid-3'},
+    { UID: 'uid-3', evaluate: evalTodo, is: 'todomvc', counter: 99, topic: 'mango' }
+  ],
+  renderers
+);
+
+function evalTodo(state){
+  return {
+    Timer: 4000,
+    enableCounting: state('Timer') === 0? !state('enableCounting'): state('enableCounting'),
+    counter: state('watching.counter') || (state('enableCounting')!==false? (!state('adding') && state('user-state.add')? state('counter')+1: state('counter')): 0),
+    topic: (typeof state('user-state.topic') === 'undefined' && state('topic')) || state('user-state.topic').toLowerCase(),
+    giphy: ((state('fetching') === undefined) || (!state('fetching') && state('user-state.loadrandompicture')))? 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + state('topic'): state('giphy'),
+    image: state('giphy.fixed_height_small_url') || state('image'),
+    loading: !state('giphy.fixed_height_small_url'),
+    adding: !!state('user-state.add'),
+    fetching: !!state('user-state.loadrandompicture')
+  };
+}
+
---
+++ examples/todomvc/rendertodo.jsx
@@ -0,0 +1,39 @@
+
+import Forest from 'forest';
+
+function renderGuiStack(state){
+  return (
+    <div>
+      <div>{state.name}</div>
+      {state.list.map((uid) => <Forest state={Forest.objects[uid]} key={uid}></Forest>)}
+    </div>);
+}
+
+function renderTodo(state,gui){
+  return (
+    <div>
+      <hr/>
+      {Object.keys(state).map((key) => (typeof(state[key]) !== 'function') && <span key={key}> | {key}: {String(state[key])} | </span>)}
+      <br/><br/>
+      {state.enableCounting===true? 'GO!': '...'}
+      <br/><br/>
+      {gui.textField('counter', 'Count')}
+      {gui.button('add','increment')}
+      <br/><br/>
+      {gui.textField('topic', 'Topic')}
+      {gui.button('loadrandompicture', 'Load picture about that')}
+      <br/><br/>
+      {state.loading? 'loading..': ''}
+      <br/><br/>
+      {gui.image('image', 'Your random image:')}
+      <br/>
+      <hr/>
+      <br/>
+    </div>);
+}
+
+export default {
+  'guistack': renderGuiStack,
+  'todomvc':  renderTodo
+};
+
