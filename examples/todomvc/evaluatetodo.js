f06a740700f2977f039789e426045c37b09b0c7c
DuncanCragg <cyrus@cilux.org>
2018-05-21 22:59:44 +0100

    fix some bugs in todo

--- examples/todomvc/evaluatetodo.js
+++ examples/todomvc/evaluatetodo.js
@@ -15,7 +15,7 @@ const uids = Forest.cacheObjects(
 function evalTodo(object){
   const todoSubmitted = !object('creating') && object('userState.newTodo-submitted')
   return Object.assign({},
-   !object('userState.newTodo-submitted') && { newTodo: object('userState.newTodo') },
+   !object('userState.newTodo-submitted') && { newTodo: object('userState.newTodo')|| '' },
     todoSubmitted                         && { newTodo: '',
                                                todos: object('todos').concat([Forest.spawnObject(
                                                  { Evaluator: evalTodoItem,
@@ -38,7 +38,7 @@ function evalTodo(object){
 
 function evalTodoItem(object){
   return Object.assign({},
-    true                        && { completed: !object('parent.clearCompleted') && object('userState.completed') },
+    true                        && { completed: !object('parent.clearCompleted') && !!object('userState.completed') },
     object('userState.destroy') && { deleted: true }
  // true                        && { editing: object('parent.editing') === object('UID') }
   );
